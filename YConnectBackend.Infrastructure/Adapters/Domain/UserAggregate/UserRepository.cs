0f21a8c2961104c24366acb61a36324558b8ce0e
Lorys Jelezian <lorysjele@gmail.com>
2021-12-01 16:04:24 +0100

    Setup Project Infrastructure features/TRE4_Api_User

---
+++ YConnectBackend.Infrastructure/Adapters/Domain/UserAggregate/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using YConnectBackend.Domain.Commons.Database;
+using YConnectBackend.Domain.Commons.UserAggregates;
+using YConnectBackend.Domain.Commons.UserAggregates.Port;
+using YConnectBackend.Infrastructure.Adapters.database;
+
+namespace YConnectBackend.Infrastructure.Adapters.Domain
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IRepository<User, YDbContext> _repository;
+
+        public UserRepository(IRepository<User, YDbContext> repository) => _repository = repository;
+
+        public async Task<User> GetUser(uint id)
+        {
+            return await _repository.FindAsync(_ => _.Id == id).ConfigureAwait(false);
+        }
+
+    }
+}
